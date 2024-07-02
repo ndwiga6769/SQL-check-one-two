ff73fae2578e9bdacc6f6ea4f922f5b294fae83f
ndwiga6769 <your.email@example.com>
2024-07-02 10:04:24 +0300

    codes

---
+++ ConfirmationRate.sql
@@ -0,0 +1,31 @@
+-- Table: Signups
+
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | user_id        | int      |
+-- | time_stamp     | datetime |
+-- +----------------+----------+
+-- user_id is the column of unique values for this table.
+-- Each row contains information about the signup time for the user with ID user_id.
+ 
+
+-- Table: Confirmations
+
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | user_id        | int      |
+-- | time_stamp     | datetime |
+-- | action         | ENUM     |
+-- +----------------+----------+
+-- (user_id, time_stamp) is the primary key (combination of columns with unique values) for this table.
+-- user_id is a foreign key (reference column) to the Signups table.
+-- action is an ENUM (category) of the type ('confirmed', 'timeout')
+-- Each row of this table indicates that the user with ID user_id requested a confirmation message at time_stamp and that confirmation message was either confirmed ('confirmed') or expired without confirming ('timeout')
+
+-- # Write your MySQL query statement below
+SELECT user_id, ROUND(AVG (action <=> 'confirmed' ),4) as confirmation_rate
+FROM Signups
+LEFT JOIN Confirmations using (user_id)
+GROUP by user_id;
